69e8c2a871972e8181f3375d18aaab2b13af7cbf
Bazurogg <bazurogg@gmail.com>
2024-09-05 11:18:31 +0200

    JAVA -> Class -> + DirectPurchase

--- src/fr/pompey/dev/afpa/model/DirectPurchase.java
+++ src/fr/pompey/dev/afpa/model/DirectPurchase.java
@@ -1,4 +1,32 @@
 package fr.pompey.dev.afpa.model;
 
-public class DirectPurchase {
+import java.util.List;
+import java.time.LocalDate;
+
+/**
+ * Represents a direct purchase in the pharmacy.
+ * Direct purchases inherit from the general Purchase class and do not have additional attributes.
+ */
+public class DirectPurchase extends Purchase {
+
+    /**
+     * Constructor for a direct purchase.
+     *
+     * @param purchaseDate The date the purchase was made
+     * @param totalPrice The total price of the purchase
+     * @param medicines The list of medicines in this purchase
+     * @param customer The customer who made the purchase
+     */
+    public DirectPurchase(LocalDate purchaseDate, double totalPrice, List<Medicine> medicines, Customer customer) {
+        super(purchaseDate, totalPrice, medicines, customer);
+    }
+
+    // Here add some futur methods implementation if needed. .
+
+    @Override
+    public String toString() {
+
+        return "DirectPurchase{" + "purchaseDate=" + getPurchaseDate() + ", totalPrice=" + getTotalPrice() + ", medicines=" + getMedicines() + ", customer=" + getCustomer() + '}';
+    }
+
 }
