ccba818307b93019bf87b8265779893b18fe385d
Bazurogg <bazurogg@gmail.com>
2024-09-06 08:40:33 +0200

    JAVA -> Swing -> Menu Home -> retail icons

--- src/fr/pompey/dev/afpa/vue/MenuHome.java
+++ src/fr/pompey/dev/afpa/vue/MenuHome.java
@@ -3,9 +3,12 @@ package fr.pompey.dev.afpa.vue;
 import javax.swing.*;
 
 public class MenuHome {
+
     private JPanel SIdeBar;
     private JButton Homebutton;
     private JButton UserButton;
     private JButton MedicineButton;
     private JButton PurchaseButton;
+
+
 }
