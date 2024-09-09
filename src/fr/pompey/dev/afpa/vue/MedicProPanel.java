18c50710e52c067c43f78692b6eee118e0d1d6dc
Bazurogg <bazurogg@gmail.com>
2024-09-09 12:13:19 +0200

    JAVA -> Swing -> Panel Doctor/Specialist -> + New Panel system

---
+++ src/fr/pompey/dev/afpa/vue/MedicProPanel.java
@@ -0,0 +1,8 @@
+package fr.pompey.dev.afpa.vue;
+
+import javax.swing.*;
+
+public class MedicProPanel {
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+}
--- src/fr/pompey/dev/afpa/vue/MenuHome.java
+++ src/fr/pompey/dev/afpa/vue/MenuHome.java
@@ -1,23 +1,33 @@
 package fr.pompey.dev.afpa.vue;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MenuHome extends JFrame {
 
     private JPanel sideBar;
     private JButton customersButton;
     private JButton medicinesButton;
-    private JButton medicProButton;
+    private JButton doctorButton;
     private JButton purchasesButton;
     private JButton homeButton;
     private JButton exitButton;
     private JPanel mainPanel;
     private JPanel displayPanel;
+    private JButton specialistButton;
 
     // Constructeur pour initialiser la fenêtre
     public MenuHome() {
         // Appelle le constructeur de JFrame pour définir le titre de la fenêtre
         super("Pharmacy Management - Home");
+
+        // Initialise displayPanel si ce n'est pas déjà fait dans le form
+        if (displayPanel == null) {
+            displayPanel = new JPanel();
+            displayPanel.setLayout(new BorderLayout()); // Important d'avoir un layout valide
+        }
 
         // Ajoute le mainPanel (qui contient ton design) à la fenêtre
         this.setContentPane(mainPanel);
@@ -29,7 +39,43 @@ public class MenuHome extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Rendre la fenêtre visible
+        this.setContentPane(mainPanel);
         this.setVisible(true);
+
+        // Ajout des écouteurs d'événements sur les boutons
+        initListeners();
+
+    }
+
+    // Initialisation des écouteurs s'évènements sur les boutons du menu
+
+    // Bouton détails Médecins et spécialistes
+    private void initListeners() {
+
+        doctorButton.addActionListener(new ActionListener() {
+
+            @Override
+
+            public void actionPerformed(ActionEvent e) {
+
+                showMedicProPanel();
+
+            }
+
+        });
+
+    }
+
+    // Méthodes pour changer le contenu du displayPanel
+
+    private void showMedicProPanel() {
+
+        displayPanel.removeAll();
+        displayPanel.setLayout(new BorderLayout());
+        displayPanel.add(new JLabel("Doctors and Specialists Management Panel"));
+        displayPanel.revalidate();
+        displayPanel.repaint();
+
     }
 
     // Main pour lancer l'application et afficher le menu d'accueil
