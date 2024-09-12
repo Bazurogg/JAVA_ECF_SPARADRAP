9a00847c67b63a1bc62f41311544d509d2acafe4
Bazurogg <bazurogg@gmail.com>
2024-09-12 13:14:02 +0200

    JAVA -> Swing -> Direct purchase panel ☑

--- src/fr/pompey/dev/afpa/vue/DirectPurchasePanel.java
+++ src/fr/pompey/dev/afpa/vue/DirectPurchasePanel.java
@@ -2,8 +2,9 @@ package fr.pompey.dev.afpa.vue;
 
 import javax.swing.*;
 
-public class DirectPurchasePanel {
-    private JPanel panelCustomer;
+public class DirectPurchasePanel extends JPanel {
+
+    private JPanel panelDirectPurchase;
     private JTextField textField3;
     private JTextField textField4;
     private JTextField textField2;
@@ -11,5 +12,16 @@ public class DirectPurchasePanel {
     private JTextField textField6;
     private JTextField textField7;
     private JTextField textField5;
-    private JButton addNewCustomerButton;
+    private JButton createNewDirectPurchaseButton;
+    private JButton cancelButton;
+
+
+    public DirectPurchasePanel() {
+
+        this.setVisible(true);
+
+        add(panelDirectPurchase);
+
+    }
+
 }
--- src/fr/pompey/dev/afpa/vue/PurchaseChoicePanel.java
+++ src/fr/pompey/dev/afpa/vue/PurchaseChoicePanel.java
@@ -1,8 +1,14 @@
 package fr.pompey.dev.afpa.vue;
 
+import fr.pompey.dev.afpa.model.DirectPurchase;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PurchaseChoicePanel extends JPanel {
+
     private JPanel panelPurchaseChoice;
     private JButton directPurchaseButton;
     private JButton byPrescriptionPurchaseButton;
@@ -13,9 +19,38 @@ public class PurchaseChoicePanel extends JPanel {
 
         add(panelPurchaseChoice);
 
+        initListeners();
+
     }
 
+    private void initListeners() {
 
+
+        directPurchaseButton.addActionListener(new ActionListener() {
+
+            @Override
+
+            public void actionPerformed(ActionEvent e) {
+
+                showDirectPurchasePanel();
+
+            }
+
+        });
+
+    }
+
+    private void showDirectPurchasePanel() {
+
+        DirectPurchasePanel directPurchasePanel = new DirectPurchasePanel();
+
+        panelPurchaseChoice.removeAll();
+
+        panelPurchaseChoice.add(directPurchasePanel, BorderLayout.CENTER);
+        panelPurchaseChoice.revalidate();
+        panelPurchaseChoice.repaint();
+
+    };
 
 
 }
