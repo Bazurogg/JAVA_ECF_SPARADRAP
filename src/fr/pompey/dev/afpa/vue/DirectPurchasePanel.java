4663896b4bcac989555d5e812e87459bccf57fcd
Bazurogg <bazurogg@gmail.com>
2024-09-12 09:59:13 +0200

    JAVA -> Swing -> Direct purchase panel -> panel redesigning

---
+++ src/fr/pompey/dev/afpa/vue/DirectPurchasePanel.java
@@ -0,0 +1,15 @@
+package fr.pompey.dev.afpa.vue;
+
+import javax.swing.*;
+
+public class DirectPurchasePanel {
+    private JPanel panelCustomer;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField2;
+    private JTextField textField1;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField5;
+    private JButton addNewCustomerButton;
+}
