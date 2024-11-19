598b218f25481f06f24a99c605389f586e4b4a9c
Bazurogg <bazurogg@gmail.com>
2024-11-19 11:06:45 +0100

    Test JDBC ➔ SELECT on customer table - OK ✔️

--- src/main/java/fr/pompey/dev/afpa/controller/Main.java
+++ src/main/java/fr/pompey/dev/afpa/controller/Main.java
@@ -2,7 +2,8 @@ package fr.pompey.dev.afpa.controller;
 
 import fr.pompey.dev.afpa.vue.MenuHome;
 
-import java.sql.Connection;
+import java.net.ConnectException;
+import java.sql.*;
 
 public class Main {
 
@@ -12,12 +13,43 @@ public class Main {
 
         try (Connection connection = dbManager.getConnection()) {
             System.out.println("Successfully connected to the database !");
+
+            Main main = new Main();
+
+            main.selectFromCustomer(connection);
+
         } catch (Exception e) {
             System.err.println("Error : " + e.getMessage());
         }
 
         // Creating an MenuHome instance when the app launching
         new MenuHome(new PurchaseManager());
+
+
+
+    }
+
+    private void selectFromCustomer(Connection connection) {
+
+        String selectQuery = "select * from customer";
+
+        try {
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery(selectQuery);
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("cu_firstname"));
+                System.out.println(resultSet.getString("cu_lastname"));
+                System.out.println("---------------------------------------------");
+            }
+
+        } catch (SQLException e) {
+
+            throw new RuntimeException(e);
+
+        }
 
     }
 
